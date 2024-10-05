c1ce8eafc65455ef65ffdbd80ae98f3171041d96
Gibo Kim <97424608+KimGiii@users.noreply.github.com>
2024-10-05 09:17:29 +0900

    Time: 39 ms (42.11%), Space: 16.5 MB (79.17%) - LeetHub

---
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            if set(s) == set(suffix):
+                return char + self.smallestSubsequence(suffix.replace(char, ''))
+
+        return ''
